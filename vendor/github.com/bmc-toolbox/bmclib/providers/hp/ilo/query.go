bcc80b5c46f214f73b75e5792716c0e7f4df6b30
Joel Rebello <joel.rebello@booking.com>
2019-03-13 17:37:59 +0100

    vendor: bump bmclib to include ilo4/5 network configuration support

--- vendor/github.com/bmc-toolbox/bmclib/providers/hp/ilo/query.go
+++ vendor/github.com/bmc-toolbox/bmclib/providers/hp/ilo/query.go
@@ -23,7 +23,7 @@ func (i *Ilo) CurrentHTTPSCert() ([]*x509.Certificate, error) {
 	conn, err := tls.DialWithDialer(dialer, "tcp", i.ip+":"+"443", &tls.Config{InsecureSkipVerify: true})
 
 	if err != nil {
-		return []*x509.Certificate{&x509.Certificate{}}, err
+		return []*x509.Certificate{{}}, err
 	}
 
 	defer conn.Close()
@@ -148,4 +148,68 @@ func (i *Ilo) queryNetworkSntp() (networkSntp NetworkSntp, err error) {
 	}
 
 	return networkSntp, err
+}
+
+func (i *Ilo) queryAccessSettings() (AccessSettings, error) {
+
+	endpoint := "json/access_settings"
+
+	var accessSettings AccessSettings
+
+	payload, err := i.get(endpoint)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"IP":       i.ip,
+			"Model":    i.BmcType(),
+			"endpoint": endpoint,
+			"step":     helper.WhosCalling(),
+			"Error":    err,
+		}).Warn("GET request failed.")
+		return accessSettings, err
+	}
+
+	err = json.Unmarshal(payload, &accessSettings)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"IP":    i.ip,
+			"step":  helper.WhosCalling(),
+			"Model": i.BmcType(),
+			"Error": err,
+		}).Warn("Unable to unmarshal payload.")
+		return accessSettings, err
+	}
+
+	return accessSettings, err
+}
+
+func (i *Ilo) queryNetworkIPv4() (NetworkIPv4, error) {
+
+	endpoint := "json/network_ipv4/interface/0"
+
+	var networkIPv4 NetworkIPv4
+
+	payload, err := i.get(endpoint)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"IP":       i.ip,
+			"Model":    i.BmcType(),
+			"endpoint": endpoint,
+			"step":     helper.WhosCalling(),
+			"Error":    err,
+		}).Warn("GET request failed.")
+		return networkIPv4, err
+	}
+
+	err = json.Unmarshal(payload, &networkIPv4)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"IP":    i.ip,
+			"step":  helper.WhosCalling(),
+			"Model": i.BmcType(),
+			"Error": err,
+		}).Warn("Unable to unmarshal payload.")
+		return networkIPv4, err
+	}
+
+	return networkIPv4, err
 }
